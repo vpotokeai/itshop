872fd7e952e5a678a1e3a2748a99c4553f29dd48
Voislav-Vpotoke <vpotoke.om@gmail.com>
2025-08-28 08:12:58 +0400

    Обновление компонентов и стилей

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,55 @@
 import React, { useState } from 'react'
-import { FaShoppingCart } from "react-icons/fa";
-import Order from './Order';
+import { FaShoppingCart } from 'react-icons/fa'
+import Order from './Order'
+import itImage from '../img/it.jpg'
 
-const showOrders = (props) => {
-  let summa = 0
-  props.orders.forEach(el => summa += Number.parseFloat(el.price))
-  return(<div>
-      {props.orders.map(el => (
-       <Order onDelete={props.onDelete} key={el.id} item={el} />
-      ))}
-      <p className='summa'>Сумма: {new Intl.NumberFormat().format(summa)}P</p>
-  </div>)
+const showOrders = props => {
+	let summa = 0
+	props.orders.forEach(el => (summa += Number.parseFloat(el.price)))
+	return (
+		<div>
+			{props.orders.map(el => (
+				<Order onDelete={props.onDelete} key={el.id} item={el} />
+			))}
+			<p className='summa'>Сумма: {new Intl.NumberFormat().format(summa)}P</p>
+		</div>
+	)
 }
 
 const showNothing = () => {
-  return(<div className='empty'>
-    <h2>Товаров нет</h2>
-  </div>)
+	return (
+		<div className='empty'>
+			<h2>Товаров нет</h2>
+		</div>
+	)
 }
 
 export default function Header(props) {
-  let [cartOpen, setCartOpen] = useState(false)
+	let [cartOpen, setCartOpen] = useState(false)
 
-  return (
-    <header>
-        <div>
-            <span className='logo'>IT Shop "КНОПКА"</span>
-            <ul className='nav'>
-                <li>Корзина</li>
-                {/* <li>Контакты</li> */}
-               
-            </ul>
-            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+	return (
+		<header>
+			<div>
+				<span className='logo'>IT Shop "КНОПКА"</span>
+				<ul className='nav'>
+					<li>Корзина</li>
+					{/* <li>Контакты</li> */}
+				</ul>
+				<FaShoppingCart
+					onClick={() => setCartOpen((cartOpen = !cartOpen))}
+					className={`shop-cart-button ${cartOpen && 'active'}`}
+				/>
 
-            {cartOpen && (
-              <div className='shop-cart'>
-                {props.orders.length > 0 ?
-                     showOrders(props) : showNothing()}
-              </div>
-            )}
-        </div>
-        <div className='presentation'>
-
-        </div>
-    </header>
-  )
+				{cartOpen && (
+					<div className='shop-cart'>
+						{props.orders.length > 0 ? showOrders(props) : showNothing()}
+					</div>
+				)}
+			</div>
+			<div
+				className='presentation'
+				style={{ backgroundImage: `url(${itImage})` }}
+			></div>
+		</header>
+	)
 }
