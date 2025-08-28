a517ee12c78510f044ef617bdb2f667c278e919d
Voislav-Vpotoke <vpotoke.om@gmail.com>
2025-08-28 07:36:19 +0400

    Обновление компонентов и стилей

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react'
 
 export class ShowFullItem extends Component {
-  render() {
-    return (
-      <div className='full-item'>
-          <div>
-          <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)}/>
-          <h2>{this.props.item.title}</h2>
-          <p>{this.props.item.desc}</p>
-          <b>{this.props.item.price}P</b>
-          <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
-          </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className='full-item'>
+				<div>
+					<div
+						className='close-button'
+						onClick={() => this.props.onShowItem(this.props.item)}
+					>
+						×
+					</div>
+					<img
+						src={'./img/' + this.props.item.img}
+						onClick={() => this.props.onShowItem(this.props.item)}
+					/>
+					<h2>{this.props.item.title}</h2>
+					<p>{this.props.item.desc}</p>
+					<b>{this.props.item.price}P</b>
+					<div
+						className='add-to-cart'
+						onClick={() => this.props.onAdd(this.props.item)}
+					>
+						+
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default ShowFullItem
